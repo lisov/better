5a97b5e02b3769ba4e3b181dbeaf52979281114b
Lisov <aleksandr.lisowski@gmail.com>
2020-08-11 19:44:58 +0300

    fixed config

--- back/app.js
+++ back/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const app = express()
 app.use('api/auth', require('./routes/auth.route'))
 const PORT = process.env.port || 5000
+// const PORT = process.env.mongoUri
 
 async function start() {
   try {
